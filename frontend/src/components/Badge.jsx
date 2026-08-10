e445757ad30a53675ceff1bc6c2a296d1c472b58
cc24151 <162441902+RafaelVasconcellos08@users.noreply.github.com>
2026-08-10 10:33:39 -0300

    Revert "Merge branch 'main' of https://github.com/viniciusgpinheiro/HydraDAST"
    This reverts commit 93afe374ec7d0ca0c9e946f85f8d1f5234f8e9f9, reversing
    changes made to 4f412a892a337e7b9a34aee21f62ba01b1d898fa.

--- frontend/src/components/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import { riscoClass, statusClass } from '../data/mock';
-
-export function RiscoBadge({ value }) {
-  return <span className={`hd-badge ${riscoClass(value)}`}>{value}</span>;
-}
-
-export function StatusBadge({ value }) {
-  return <span className={`hd-badge ${statusClass(value)}`}>{value}</span>;
-}
